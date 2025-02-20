aad3ace89161198bc1e93cfca8b0f410e0a609e9
rajatraj3333 <rajatkumar108@hotmail.com>
2025-02-20 23:13:44 +0530

    adding static end point

--- backend/src/server.js
+++ backend/src/server.js
@@ -89,6 +89,14 @@ app.get("/send", async (req,res) => {
 
   res.status(200).json({ data });
 });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
